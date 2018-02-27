ea970e80843dddc1f8db8d5f711f7ffb321042fe
Garett Gelsinger <garett.gelsinger@gmail.com>
2018-02-26 21:39:39 -0600

    render burger ingredients based on state

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,14 +4,22 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    const transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+               return <BurgerIngredient key={igKey + i} type={igKey} />
+            });
+        });
+
+
+    
+    
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="bacon" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="lettuce" />
-            <BurgerIngredient type="meat" />
+                {transformedIngredients}
             <BurgerIngredient type="bread-bottom" />
+
         </div>
     );
 };
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,10 +4,18 @@ import Aux from '../../hoc/Auxs';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+    state = {
+        ingredients: {
+            lettuce: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2
+        }    
+    }
     render() {
         return(
             <Aux>
-                <Burger/>
+                <Burger ingredients={this.state.ingredients}/>
                 <div>Build Controls</div>
             </Aux>
         );
