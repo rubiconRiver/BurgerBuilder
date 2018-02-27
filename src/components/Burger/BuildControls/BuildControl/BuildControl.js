a4dfd2f3ef1f9fd10f64698e5cc306649696ca9f
Garett Gelsinger <garett.gelsinger@gmail.com>
2018-02-27 06:50:58 -0600

    finished the build control component, next step is to add them

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -4,10 +4,10 @@ import classes from './BuildControl.css'
 
 const buildControl = () => {
     return (
-        <div>
-            <div>{props.label}</div>
-            <button>Less</button>
-            <button>More</button>
+        <div className={classes.BuildControl}>
+            <div className={classes.Label}>{props.label}</div>
+            <button className={classes.Label}>Less</button>
+            <button className={classes.Label}>More</button>
         </div>
     );
 };
