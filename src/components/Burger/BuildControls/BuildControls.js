67cef92d8a5b0caefe23fc9257cd4b305374b929
rubiconriver <garett.gelsinger@gmail.com>
2018-02-27 19:38:05 -0600

    added skeleton for controls

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,11 +1,23 @@
 import React from 'react';
 
-const buildControls = (props) => {
+import classes from './BuildControls.css'
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+    {label: 'Lettuce', type: 'lettuce'},
+    {label: 'Bacon', type: 'bacon'},
+    {label: 'Cheese', type: 'cheese'},
+    {label: 'Meat', type: 'meat'},
+];
+
+const buildControls = () => {
     return (
-        <div>
-            
+        <div className={classes.BuildControls}>
+            {controls.map(ctrl => {
+              return  <BuildControl key={ctrl.label} label={ctrl.label}/>
+            })}
         </div>
     );
 };
 
-export default buildControls;props
+export default buildControls;
