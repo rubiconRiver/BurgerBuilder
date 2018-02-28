cf9281660f16a8df123a78dd37a8aecebb453f3b
rubiconriver <garett.gelsinger@gmail.com>
2018-02-27 21:21:04 -0600

    added remove button functionality (with disable)

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -39,28 +39,46 @@ class BurgerBuilder extends Component {
         this.setState({ingredients: updatedIngredients, totalPrice: newPrice});
     }
 
-    // removeIngredientHandler = (type) => {
-    //     const oldCount = this.state.ingredients[type];
-    //     const updatedCount = oldCount + 1;
-    //     const updatedIngredients = {
-    //         ...this.state.ingredients.type
-    //     };
-    //     updatedIngredients[type] = updatedCount;
+    removeIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        if (oldCount <= 0) {
+            return;
+        }
+        const updatedCount = oldCount - 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updatedCount;
 
-    //     //update total price
-    //     const priceAddition = INGREDIENT_PRICES[type];
-    //     const oldPrice = this.state.totalPrice;
-    //     const newPrice = oldPrice + priceAddition;
+        //update total price
+        const priceReduction = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice - priceReduction;
 
-    //     this.setState({ingredients: updatedIngredients, totalPrice: newPrice});
-    // }
+        this.setState({ingredients: updatedIngredients, totalPrice: newPrice});
+    }
 
     render() {
+        //check if button should be disabled
+        const disabledInfo = {
+            ...this.state.ingredients
+        };
+
+        //for each item, set disabledInfo to true if the value is <= 0
+        for (let key in disabledInfo) {
+            disabledInfo[key] = disabledInfo[key] <= 0
+        }
+
         return(
             <Aux>
+                <p>{this.state.totalPrice}</p>
                 <Burger ingredients={this.state.ingredients}/>
+                
                 <BuildControls 
-                    ingredientAdded={this.addIngredientHandler}/>
+                    ingredientAdded={this.addIngredientHandler}
+                    ingredientRemoved={this.removeIngredientHandler}
+                    disabled={disabledInfo}/>
+                
             </Aux>
         );
     }
