ccf25f9499c82bcbf566ab1cd7a6b50b568293b9
rubiconriver <garett.gelsinger@gmail.com>
2018-03-03 17:23:33 -0600

    added sidebar and changed logo size

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -8,7 +8,9 @@ const toolbar = (props) => {
     return (
         <header className={classes.Toolbar}>
             <div>MENU</div>
+            <div className={classes.Logo}>
                 <Logo />
+            </div>
                     <nav>
                         <NavigationItems/>
                     </nav>
