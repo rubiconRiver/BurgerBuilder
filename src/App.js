1e408f1662dcad028d69eff77fa573fb12639aa1
rubiconRiver <garett.gelsinger@gmail.com>
2018-03-09 12:30:27 -0600

    added eject function to ComponentWillUnmount

--- src/App.js
+++ src/App.js
@@ -4,11 +4,16 @@ import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 
 class App extends Component {
+  state = {
+    show: true
+  }
+
+  
   render() {
     return (
       <div>
         <Layout>
-          <BurgerBuilder/>
+           <BurgerBuilder/>
         </Layout>
       </div>
     );
